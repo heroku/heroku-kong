f6d59df165570657bc1998cfe7539aceded48058
Mars Hall <mars@heroku.com>
2016-01-06 15:41:42 -0800

    Working "hello-world-header" example with docs.

---
+++ lib/kong/plugins/hello-world-header/handler.lua
@@ -0,0 +1,17 @@
+local BasePlugin = require "kong.plugins.base_plugin"
+local HelloWorld = BasePlugin:extend()
+
+local date = require "date"
+
+function HelloWorld:new()
+  HelloWorld.super.new(self, "hello-world-header")
+end
+
+function HelloWorld:header_filter(config)
+  HelloWorld.super.header_filter(self)
+
+  local now = date()
+  ngx.header["X-Hello-World"] = "Today is "..now:fmt("%F")
+end
+
+return HelloWorld
