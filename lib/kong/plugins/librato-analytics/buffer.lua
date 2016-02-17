561f131023e10f4161ab8ad1f89f37ed6e5ff29b
Mars Hall <mars@heroku.com>
2016-02-17 15:39:44 -0800

    Fix `librato-analytics` plugin's shared memory dict in Kong 0.6

--- lib/kong/plugins/librato-analytics/buffer.lua
+++ lib/kong/plugins/librato-analytics/buffer.lua
@@ -33,7 +33,7 @@ local setmetatable = setmetatable
 -- The policy will give a delay that grows everytime
 -- Galileo fails to respond. As soon as Galileo responds,
 -- the delay is reset to its base.
-local dict = ngx.shared.locks
+local dict = ngx.shared.cache
 local RETRY_INDEX_KEY = "librato_analytics_retry_index"
 local RETRY_BASE_DELAY = 1 -- seconds
 local RETRY_MAX_DELAY = 60 -- seconds
